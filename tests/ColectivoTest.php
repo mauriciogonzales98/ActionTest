550eab07de2b96b6f5eb30e0dc30dd6ff6763ddf
mauricio gonzales <mauri9825@gmail.com>
2023-08-21 21:45:40 -0300

    added new test

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -8,5 +8,7 @@ class ColectivoTest extends TestCase{
    public function testGetLinea(){
         $cole = new Colectivo(129);
         $this->assertEquals($cole->getLinea(), 129);
+
+        $this->assertNotEquals($cole->getLinea(), 130);
     }
 }
